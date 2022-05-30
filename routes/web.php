ae16a6c4a93b1346dcfcade84aef6054dfeee373
EC2 Default User <ec2-user@ip-172-31-26-204.ec2.internal>
2022-05-30 05:24:35 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/',[WelcomeController::class,'index']);
+Route::get('/welcome',[WelcomeController::class,'index']);
 Route::get('/welcome/second',[WelcomeController::class,'second']);
