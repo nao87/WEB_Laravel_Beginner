db33ebf060cff2ba200b557c91bea302ed947655
EC2 Default User <ec2-user@ip-172-31-26-204.ec2.internal>
2022-06-06 09:46:43 +0000

    タスクの編集を書いた

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,15 @@ Route::post('/test/input',[TestController::class,'input']);
 //タスク管理システム
 Route::get('/',[AuthController::class,'index'])->name('front.index');
 Route::post('/login',[AuthController::class,'login']);
-//認可処理
-Route::middleware(['auth'])->group(function(){
-  Route::prefix('/task')->group(function(){
-    Route::get('/list',[TaskController::class,'list']);
-    Route::post('/register',[TaskController::class,'register']);
-    Route::get('/detail/{task_id}',[TaskController::class,'detail'])->whereNunmber('task_id')->name('detail');
-  });
-    Route::get('/logout',[AuthController::class,'logout']);
+// 認可処理
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('/task')->group(function () {
+        Route::get('/list', [TaskController::class, 'list']);
+        Route::post('/register', [TaskController::class, 'register']);
+        Route::get('/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
+        Route::get('/edit/{task_id}', [TaskController::class, 'edit'])->whereNumber('task_id')->name('edit');
+        Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');
+    });
+    //
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
