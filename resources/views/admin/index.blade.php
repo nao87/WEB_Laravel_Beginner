434b50c3be0b5538856e1d2dd6feba762761b2ea
EC2 Default User <ec2-user@ip-172-31-26-204.ec2.internal>
2022-06-09 05:11:33 +0000

    管理画面デザインにBootstrap5を入れた

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,6 +13,6 @@
             @csrf
             ログインID:<input name="login_id"><br>
             パスワード:<input name="password" type="password"><br>
-            <button>ログインする</button>
+            <button class="btn btn-primary mb-3">ログインする</button>
         </form>
 @endsection
